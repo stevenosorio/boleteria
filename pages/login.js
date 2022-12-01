d8ac27d98a110aa6668fa79e9a2b4e9fe1a42a5a
ahmed zaghlout <84550594+ahmedzaghlout@users.noreply.github.com>
2022-12-01 18:54:08 -0500

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
  
 export default function Home() {  
   return ( 
-    <div>
+    <div class= "bg-slate-500">
         <header>
           <h1>Navegacion</h1>
         </header>
 
         <main class="" >
-          <div  class="flex flex-col mx-80 my-20 rounded-lg border-x-4 border-sky-500 bg-sky-500   ">
-          <img class="max-w-l place-content-center" src="./images/latiquetera-logo.png" alt=""/>
+          <div  class="flex flex-col mx-80  rounded-lg border-x-4 border-sky-500 bg-white   ">
+          <img class="max-w-xs max-h-xs mx-40 place-content-center" src="./images/boleteria-logo.png" alt=""/>
             <p class="max-w-xl place-content-center" >Inicia sesión en tu Cuenta</p>
             <form  class="flex flex-col  my-5">
               <input class="" type="text" name="nombre" placeholder="Nombre" id="nombre"/>
